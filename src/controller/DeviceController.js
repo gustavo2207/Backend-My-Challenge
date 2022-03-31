c209badf56254846872eff748df8955c06dd3d61
Gustavo Moraes Mercedes <gustavomercedes1@hotmail.com>
2022-03-31 00:51:27 -0300

    integration

--- src/controller/DeviceController.js
+++ src/controller/DeviceController.js
@@ -16,11 +16,11 @@ module.exports = {
 
     async store(req, res) {
         const { category_name } = req.params;
-        const { color, partNumber } = req.body;
+        const { color, part_number } = req.body;
 
         const isExist = await Device.findOne({
             where: {
-                part_number: partNumber
+                part_number: part_number
             }
         });
 
@@ -28,36 +28,33 @@ module.exports = {
 
         let category = await Category.findOne({where: {name: `${category_name}`}});
 
-        
-        console.log(color, partNumber)
-
         if(!category) return res.status(400).json({error: 'Category no exists'})
 
 
         const device = await Device.create({
             color: color,
-            part_number: partNumber,
+            part_number: part_number,
             id_category: category.id
         })
         return res.json(device);
     },
 
     async delete(req, res) {
-        const {partNumber} = req.params;
+        const {part_number} = req.params;
 
         const device = await Device.findOne({
             where: {
-                part_number: partNumber
+                part_number: part_number
             }
         });
 
         if(!device) return res.status(404).json({
-            error: `No one devices has this part number: ${partNumber}`
+            error: `No one devices has this part number: ${part_number}`
         })
 
         await Device.destroy({
             where: {
-                part_number: partNumber
+                part_number: part_number
             }
         });
 
