48d09259c854c097a8d0446b6a28afc3f5e4f16f
Gustavo Moraes Mercedes <gustavomercedes1@hotmail.com>
2022-04-01 02:04:57 -0300

    sync aws

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,12 @@
 module.exports = {
     dialect: "mysql",
-    host: "localhost",
-    username: "root",
+    host: "mysqlserver.cnrgtg2a5fub.us-east-1.rds.amazonaws.com",
+    username: "admin",
     port: "3306",
-    password: "espiaodaCiA572@",
+    password: "admin1234",
     database: "datamychallenge",
     define: {
         timestamps: true,
         underscored: true
     }
-    
 }
