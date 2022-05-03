cc3880f588d727d4af5ef38d55b6a7270dfd313e
Gustavo Moraes Mercedes <gustavomercedes1@hotmail.com>
2022-05-03 17:38:07 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -8,14 +8,9 @@ require('./database')
 
 const app = express()
 
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "http://mychallengefrontend.s3-website-sa-east-1.amazonaws.com");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    app.use(cookieParser())
-    app.use(express.json())
-    app.use(routes)
-    app.listen(3333)
-})
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
