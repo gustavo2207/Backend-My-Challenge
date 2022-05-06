f6b95b47f2013f5f83cfed925f413640d8211cf6
Gustavo Moraes Mercedes <gustavomercedes1@hotmail.com>
2022-05-06 13:51:52 -0300

    cors

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,25 @@
 module.exports = {
-    dialect: "mysql",
-    host: "mysqlserver.cnrgtg2a5fub.us-east-1.rds.amazonaws.com",
-    username: "admin",
-    port: "3306",
-    password: "admin1234",
-    database: "datamychallenge",
-    define: {
-        timestamps: true,
-        underscored: true
-    }
-}
+  dialect: "mysql",
+  host: "mysqlserver.cnrgtg2a5fub.us-east-1.rds.amazonaws.com",
+  username: "admin",
+  port: "3306",
+  password: "admin1234",
+  database: "datamychallenge",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
+/* module.exports = {
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  port: "3306",
+  password: "espiaodaCiA572@",
+  database: "datamychallenge",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
+ */
--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const routes = require('./routes.js')
+const express = require("express");
+const routes = require("./routes.js");
 
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-require('./database')
+require("./database");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(cookieParser())
-app.use(express.json())
-app.use(routes)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(routes);
+  next();
+});
 
-app.listen(3333)
+app.listen(3333);
