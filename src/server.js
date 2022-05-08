2c95fb164f1389dffc09e99ead2f5fa5c1905f39
Gustavo Moraes Mercedes <gustavomercedes1@hotmail.com>
2022-05-08 00:59:42 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use((req, res, next) => {
   cookieParser();
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://mychallengefrontend.s3-website-sa-east-1.amazonaws.com/"
+  );
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   cors();
   express.json();
